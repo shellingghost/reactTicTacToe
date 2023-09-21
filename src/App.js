bc0bab385d8123d0ca5dfb4410fbb2aa7882f253
shellingghost <97666032+shellingghost@users.noreply.github.com>
2023-09-21 16:39:58 -0700

    Update App.js
    Updated <> to <React.Fragment>

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@ import React from 'react';
 
 export default function Square() {
   return (
-    <>
+    <React.Fragment>
       <button className="square">X</button>
       <button className="square">X</button>
-    </>
+    </React.Fragment>
   );
-
 }
