069be557d44fb0e5cfd5a22892ec1bba5f5d5368
shellingghost <97666032+shellingghost@users.noreply.github.com>
2023-09-23 12:49:58 -0700

    Update App.js
    Created button component and a handler for clicks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
 import React from 'react';
 
-export default function Square() {
+function Square({value}) {
+  // nested function, not returning
+  //    console call
+  function handleClick() {
+    console.log('clicked!');
+  }
+
+  return (
+    <button
+      //properties of button- JSX component
+      className="square"
+      //passing function 
+      //escaping JSX into javascript using braces
+
+      //scope* - interesting
+      //init var with js function
+      // onClick/handleClick convention
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
+}
+
+export default function Board() {
   return (
     <React.Fragment>
-      <button className="square">X</button>
-      <button className="square">X</button>
+      <div className= "board-row">
+        <Square value="1"/>
+        <Square value="2"/>
+        <Square value="3"/>
+      </div>
+      
+      <div className= "board-row">
+        <Square value="4"/>
+        <Square value="5"/>
+        <Square value="6"/>
+      </div>
+
+      <div className= "board-row">
+        <Square value="7"/>
+        <Square value="8"/>
+        <Square value="9"/>
+      </div>
+
     </React.Fragment>
   );
 }
