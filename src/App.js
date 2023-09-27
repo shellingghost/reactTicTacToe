c9a07b5f2d756df32b06d45e9985161b7aa61cb3
shellingghost <97666032+shellingghost@users.noreply.github.com>
2023-09-27 12:33:01 -0700

    Update App.js
    created state variable and in process of utilizing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useState } from 'react';
 
-function Square({value}) {
+function Square() {
+  const [value, setValue] = useState(null);
+
   // nested function, not returning
   //    console call
   function handleClick() {
-    console.log('clicked!');
+    setValue('X');
   }
 
   return (
@@ -25,24 +28,28 @@ function Square({value}) {
 }
 
 export default function Board() {
+  // (state variable) defaults 9 null arr
+  // board state - held and ref
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  
   return (
     <React.Fragment>
       <div className= "board-row">
-        <Square value="1"/>
-        <Square value="2"/>
-        <Square value="3"/>
+        <Square value={squares[0]} />
+        <Square value={squares[1]}/>
+        <Square value={squares[2]}/>
       </div>
       
       <div className= "board-row">
-        <Square value="4"/>
-        <Square value="5"/>
-        <Square value="6"/>
+        <Square value={squares[3]}/>
+        <Square value={squares[4]}/>
+        <Square value={squares[5]}/>
       </div>
 
       <div className= "board-row">
-        <Square value="7"/>
-        <Square value="8"/>
-        <Square value="9"/>
+        <Square value={squares[6]}/>
+        <Square value={squares[7]}/>
+        <Square value={squares[8]}/>
       </div>
 
     </React.Fragment>
